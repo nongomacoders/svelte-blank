a6bdfa6aee4d5b7b0293b2df82c28a64bc6830b2
asusdion <dr.dtdavan@gmail.com>
2020-11-25 15:16:47 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Heading from './Heading.svelte'
+	let days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"];
+
+</script>
+<Heading/>
+<main>
+{#each days as day}
+<button class="btn blue">{day}</button>
+<br>
+{/each}
+
+
+</main>
+
+<style>
+
+</style>
---
+++ src/Heading.svelte
@@ -0,0 +1,11 @@
+<script>
+let title="K53"
+</script>
+
+<div>{title}</div>
+
+<style>
+    div{
+        font-size: 4rem;
+        text-align: center;}
+</style>
